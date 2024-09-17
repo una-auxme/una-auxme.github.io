d275536452b2f93a9afa0ee9c0a6ae4968fb0033
nilsmandischer <nilsmandischer@users.noreply.github.com>
2024-09-17 11:46:12 +0000

    Deploy site built from commit cab3e8cd71cc3fa5b2c4f38a241c5fa1a7a9fd95

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "Konditionierung von Diffusion Modellen",
+        "excerpt":"Du findest generative Modelle wie Stable Diffusion oder DALL-E, die anhand von Text detailreiche Bilder erzeugen können, spannend und hast Interesse, dich in aktuelle Machine-Learning-Themen einzuarbeiten? In dieser Arbeit hast du die Möglichkeit, Methoden wie Stable Diffusion und SV3D genauer kennenzulernen. Stable Diffusion ermöglicht es, Bilder anhand von Text oder...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/Diffusion.html",
+        "teaser": null
+      },{
+        "title": "Fine-tuning von Large Reconstruction Models für die 3D Objektgenerierung",
+        "excerpt":"Kann man per Knopfdruck 3D-Objekte erzeugen? Generative Modelle wie Stable Diffusion oder DALL-E können bereits beeindruckende Bilder anhand einer Beschreibung auf Knopfdruck erzeugen. Das wäre auch für 3D-Objekte interessant, und tatsächlich lassen sich Methoden der Bildgenerierung auch zur Erzeugung von 3D-Objekten nutzen. Allerdings sind die Ergebnisse oft noch nicht so...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/LRM.html",
+        "teaser": null
+      },{
+        "title": "Vergleich von Loss Funktionen bei KI-getriebenen Partikelsimulationen",
+        "excerpt":"Partikelmethoden unterscheiden sich von anderen Machine Learning-Ansätzen durch ihr chaotisches Verhalten. Klassiche Distanzmethoden wie der Mean Squared Error können mit diesem Umstand nicht gut umgehen. Beispielsweise können Partikel innerhalb eines Wassertröpfchens sich chaotisch bewegen, obwohl die insgesamte Geometrie sich nicht verändert. Dadurch entsteht eine Diskrepanz zwischen Trainingsdaten und gelernten Partikeln,...","categories": [],
+        "tags": ["pm","fm","ba","ma"],
+        "url": "/kircher/Loss_functions.html",
+        "teaser": null
+      },{
+        "title": "GPU Nachbarschaftssuche für Partikel",
+        "excerpt":"Ich befasse mich mit der Verbesserung von KI-gestützter Simulation von großen Partikelsystemen. Mein Arbeit basiert dabei auf dem Paper von Sanchez et al. (Link unter dem Foto). Ein wichtiger Bestandteil dabei ist die Nachbarschaftssuche, die für den Aufbau des Graphen benötigt wird. Dieser Schritt ist zu jedem Simulationszeitpunkt notwendig und...","categories": [],
+        "tags": ["pm","fm","ba","ma"],
+        "url": "/kircher/Neighborhood.html",
+        "teaser": null
+      },{
+        "title": "Visualsierung von Simulationsergebnissen in Julia",
+        "excerpt":"In Computational Fluid Dynamic ist es notwendig die Vielzahl von Daten zu visualisieren, um alle relevanten Ergebnisse aus den Daten zu extrahieren. Das gängigste Dateiformat ist hierbei VTK. Den Graph Network-based simulators fehlt dieses wichtige und mächtige Tool jedoch noch. Dein Auftrag, wenn du dich entschließt ihn anzunehmen, lautet: Mögliches...","categories": [],
+        "tags": ["sb","sm"],
+        "url": "/kircher/Visualisierung.html",
+        "teaser": null
+      },{
+        "title": "Seminararbeit zur Falsifizierung und virtuellen Validierung autonomer Fahrzeuge",
+        "excerpt":"Im Rahmen dieser Seminararbeit sollen Sie einen umfassenden Überblick über moderne Methoden zur Falsifizierung und virtuellen Validierung von autonomen Fahrzeugen erarbeiten. Der besondere Fokus liegt dabei auf der Anwendung von Deep Reinforcement Learning (DRL) in diesem Kontext. Beginnen Sie Ihre Arbeit mit einer Einführung in die Herausforderungen, die bei der...","categories": [],
+        "tags": ["sm"],
+        "url": "/luttkus/SA_falsifizierungs_methoden.html",
+        "teaser": null
+      },{
+        "title": "Integration von Robot-SF in SLURM-Umgebung",
+        "excerpt":"Im Rahmen dieses Projektmoduls soll die Trainingsumgebung Robot-SF für eine effiziente Nutzung auf unserem Servercluster optimiert werden. Robot-SF ist eine Simulationsumgebung, in der ein Roboter mittels Reinforcement Learning (RL) trainiert wird, sich sicher in einer 2D-Umgebung mit Fußgängern zu bewegen. Da RL-Methoden sehr ressourcenintensiv sind, ist es das Ziel dieses...","categories": [],
+        "tags": ["pm"],
+        "url": "/luttkus/SLURM_robot_sf.html",
+        "teaser": null
+      },{
+        "title": "Praktikum Autonomes Fahren Refactoring",
+        "excerpt":"Das Praktikum Autonomes Fahren findet in jedem Wintersemester statt. Wenn du bereits an dem Projekt gearbeitet hast, kannst du auch im Sommersemester weiter daran arbeiten. PAF  ","categories": [],
+        "tags": ["pm","hiwi"],
+        "url": "/luttkus/PAF.html",
+        "teaser": null
+      },{
+        "title": "Vorhersage von Leistungsfähigkeit von Menschen durch GPT Modelle",
+        "excerpt":"Am Lehrstuhl für Mechatronik untersuchen wir Mensch-Roboter Interaktion von einem besonderen Blickwinkel. Jeder Mensch besitzt Fähigkeiten. Diese lassen sich bereits über arbeitsmedizinische Dokumentationsverfahren bewerten. Gleichsam lassen sich Anforderungen an einen Arbeitsprozess definieren. Über den Vergleich von Fähigkeiten und Anforderungen lässt sich dann auswerten, in welchen Bereichen eine Person eingesetzt werden...","categories": [],
+        "tags": ["ma"],
+        "url": "/mandischer/capability_gpt.html",
+        "teaser": null
+      },{
+        "title": "Kombination von Arbeitmedizin und KI zur Vorhersage menschlicher Fähigkeiten",
+        "excerpt":"In meiner Forschung beschäftige ich mich mit der Vorhersage von menschlichen Fähigkeiten, um damit Robotervrhaltne zu planen und insbesondere Menschen mit Behinderung in ihrem Arbeitsalltag zu unterstützen. Dabei werden Methoden aus der Arbeitsmedizin und dem Maschinellen Lernen kombiniert. Ziel ist, dass eines Tages Personen egal welcher körperlichen Fähigkeiten und Voraussetzungen...","categories": [],
+        "tags": ["pm","fm","ba","ma","hiwi"],
+        "url": "/mandischer/human_capabilities.html",
+        "teaser": null
+      },{
+        "title": "Virtuelle Lerndaten für die soziale Mensch-Roboter-Interaktion - Modellierung und Simulation von Arbeitsprozessen mit Menschen mit Behinderung",
+        "excerpt":"In meiner Forschung beschäftige ich mich mit der Vorhersage von menschlichen Fähigkeiten, um damit Robotervrhaltne zu planen und insbesondere Menschen mit Behinderung in ihrem Arbeitsalltag zu unterstützen. Dabei werden Methoden aus der Arbeitsmedizin und dem Maschinellen Lernen kombiniert. Ziel ist, dass eines Tages Personen egal welcher körperlichen Fähigkeiten und Voraussetzungen...","categories": [],
+        "tags": ["sm","sb","pm","fm","ba","ma","hiwi"],
+        "url": "/mandischer/human_modelling.html",
+        "teaser": null
+      },{
+        "title": "Evaluation neuer Use-Cases mit Graph Neural Networks",
+        "excerpt":"Unser Softwarepaket MeshGraphNets.jl wurde bereits erfolgreich auf verschiedene Use-Cases (z.B. eine Fahrzeugkabine oder eine hydraulische Bremse) innerhalb unserer Projekte angewendet. Hier enden jedoch die Möglichkeiten für die Anwendung nicht, da viele weitere Domänen ähnliche Problemstellungen besitzen.   Mögliche Anwendungsfälle:      Medizin (z.B. kardiovasuläres System)   Energienetze (z.B. Windkraftanlagen)   …  ","categories": [],
+        "tags": ["sb","sm","fm","pm","ba","ma"],
+        "url": "/trommer/datasets.html",
+        "teaser": null
+      },{
+        "title": "Weiterentwicklung unserer Softwarepakete für Graph Neural Networks",
+        "excerpt":"Die Programmiersprache Julia wächst aktuell stark in ihrer Anwendung sowohl in der Forschung als auch in der Industrie bei großen Unternehmen wie beispielsweise ASML und Bosch. Dabei arbeitet unser Lehrstuhl aktiv an der (Weiter-)entwicklung unser aktuellen Softwarepakete für Julia, besonders im Bereich Graph Neural Networks. Dabei gibt es immer aktuelle...","categories": [],
+        "tags": ["sb","sm","hiwi"],
+        "url": "/trommer/software.html",
+        "teaser": null
+      },]
