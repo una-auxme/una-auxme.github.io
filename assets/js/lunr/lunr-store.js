ed80b00f4c9da76da17fde24eb99081f74d8a072
nilsmandischer <nilsmandischer@users.noreply.github.com>
2025-02-07 12:06:16 +0000

    Deploy site built from commit 713cf4ff03c56a7ca3edd9fdbc2795eb3e167504

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "Konditionierung von Diffusion Modellen",
+        "excerpt":"Du findest generative Modelle wie Stable Diffusion oder DALL-E, die anhand von Text detailreiche Bilder erzeugen können, spannend und hast Interesse, dich in aktuelle Machine-Learning-Themen einzuarbeiten? In dieser Arbeit hast du die Möglichkeit, Methoden wie Stable Diffusion und SV3D genauer kennenzulernen. Stable Diffusion ermöglicht es, Bilder anhand von Text oder...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/Diffusion.html",
+        "teaser": null
+      },{
+        "title": "Fine-tuning von Large Reconstruction Models für die 3D Objektgenerierung",
+        "excerpt":"Kann man per Knopfdruck 3D-Objekte erzeugen? Generative Modelle wie Stable Diffusion oder DALL-E können bereits beeindruckende Bilder anhand einer Beschreibung auf Knopfdruck erzeugen. Das wäre auch für 3D-Objekte interessant, und tatsächlich lassen sich Methoden der Bildgenerierung auch zur Erzeugung von 3D-Objekten nutzen. Allerdings sind die Ergebnisse oft noch nicht so...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/LRM.html",
+        "teaser": null
+      },{
+        "title": "Benchmarkprobleme für Partikelsimulationen",
+        "excerpt":"Ich befasse mich mit der Verbesserung von KI-gestützter Simulation von großen Partikelsystemen. Das Feld der Fluidsimulationen ist vielseitig und umfasst eine große Anzahl an Anwendungsfällen und Szenarien, die in Papern verwendet werden, um Methoden zu validieren und vergleichen. Etablierte Benchmarkprobleme, die überall Anwendung finden und verschiedene Komplexitätsebenen abbilden gibt es...","categories": [],
+        "tags": ["fm","ba","hiwi"],
+        "url": "/kircher/Datengenerierung.html",
+        "teaser": null
+      },{
+        "title": "Vorhersage von Leistungsfähigkeit von Menschen durch GPT Modelle",
+        "excerpt":"Am Lehrstuhl für Mechatronik untersuchen wir Mensch-Roboter Interaktion von einem besonderen Blickwinkel. Jeder Mensch besitzt Fähigkeiten. Diese lassen sich bereits über arbeitsmedizinische Dokumentationsverfahren bewerten. Gleichsam lassen sich Anforderungen an einen Arbeitsprozess definieren. Über den Vergleich von Fähigkeiten und Anforderungen lässt sich dann auswerten, in welchen Bereichen eine Person eingesetzt werden...","categories": [],
+        "tags": ["ma"],
+        "url": "/mandischer/capability_gpt.html",
+        "teaser": null
+      },{
+        "title": "Kombination von Arbeitsmedizin und KI zur Vorhersage menschlicher Fähigkeiten",
+        "excerpt":"In meiner Forschung beschäftige ich mich mit der Vorhersage von menschlichen Fähigkeiten, um damit Robotervrhaltne zu planen und insbesondere Menschen mit Behinderung in ihrem Arbeitsalltag zu unterstützen. Dabei werden Methoden aus der Arbeitsmedizin und dem Maschinellen Lernen kombiniert. Ziel ist, dass eines Tages Personen egal welcher körperlichen Fähigkeiten und Voraussetzungen...","categories": [],
+        "tags": ["pm","fm","ba","ma"],
+        "url": "/mandischer/human_capabilities.html",
+        "teaser": null
+      },{
+        "title": "Biases und Muster in der Bewertung menschlicher Fähigkeiten",
+        "excerpt":"Maßnahmen in der Rehabilitation werden in Form von Fähigkeitsprofilen dokumentiert, die den Therapieverlauf der Patienten wiedergeben. Ein verbreitetes Verfahren dazu ist IMBA (Integration von Menschen mit Behinderung in die Arbeitswelt). Wir vermuten, dass es innerhalb des Verfahrens versteckte Abhängigkeiten und Biases gibt. Daher soll in dieser Arbeit ein Datensatz von...","categories": [],
+        "tags": ["pm","fm"],
+        "url": "/mandischer/imba_data.html",
+        "teaser": null
+      },{
+        "title": "HiWi für Mensch-Roboter-Kollaboration mit Menschen mit Behinderung",
+        "excerpt":"In meiner Forschung beschäftige ich mich mit der Vorhersage von menschlichen Fähigkeiten, um damit Robotervrhaltne zu planen und insbesondere Menschen mit Behinderung in ihrem Arbeitsalltag zu unterstützen. Dabei werden Arbeitsmedizin und datengetriebene Methoden kombiniert. Ziel ist, dass eines Tages Personen egal welcher körperlichen Fähigkeiten und Voraussetzungen gemeinsam am Arbeitsalltag teilhaben...","categories": [],
+        "tags": ["hiwi"],
+        "url": "/mandischer/hiwi_capabilities.html",
+        "teaser": null
+      },{
+        "title": "SciML against AML",
+        "excerpt":"Jeder sechste Mensch stirbt laut WHO an Krebs. Dem gegenüber steht der stetige wissenschalftliche Fortschritt auf dem Gebiet der Krebsforschung, sodass viele Krebsarten geheilt werden können wenn sie frühzeitig erkannt und effektiv behandelt werden. Der Lehrstuhl Mechatronik startet 2025 zusammen mit dem Universitätsklinikum Augsburg Forschung im Bereich der Akuten myeloischen...","categories": [],
+        "tags": ["sb","sm"],
+        "url": "/thummerer/aml.html",
+        "teaser": null
+      },{
+        "title": "Software für Scientific Machine Learning (SciML)",
+        "excerpt":"Wenn mächtige, aber schwerfällige Simulationsmodelle aus der Industrie und nagelneue, flinke Methoden aus dem maschinellen Lernen zusammen kommen, dann kollidideren Welten - dank FMI.jl und FMIFlux.jl bekommt man davon aber (fast) nichts mit. Damit das so bleibt - und wir im Idealfall das “fast” im vorhergehenden Satz bald komplett streichen...","categories": [],
+        "tags": ["hiwi","pm","fm"],
+        "url": "/thummerer/sciml.html",
+        "teaser": null
+      },{
+        "title": "Evaluation neuer Use-Cases mit Graph Neural Networks",
+        "excerpt":"Unser Softwarepaket MeshGraphNets.jl wurde bereits erfolgreich auf verschiedene Use-Cases (z.B. eine Fahrzeugkabine oder eine hydraulische Bremse) innerhalb unserer Projekte angewendet. Hier enden jedoch die Möglichkeiten für die Anwendung nicht, da viele weitere Domänen ähnliche Problemstellungen besitzen.   Mögliche Anwendungsfälle:      Medizin (z.B. kardiovasuläres System)   Energienetze (z.B. Windkraftanlagen)   …  ","categories": [],
+        "tags": ["sb","sm","fm","pm","ba","ma"],
+        "url": "/trommer/datasets.html",
+        "teaser": null
+      },{
+        "title": "Weiterentwicklung unserer Softwarepakete für Graph Neural Networks",
+        "excerpt":"Die Programmiersprache Julia wächst aktuell stark in ihrer Anwendung sowohl in der Forschung als auch in der Industrie bei großen Unternehmen wie beispielsweise ASML und Bosch. Dabei arbeitet unser Lehrstuhl aktiv an der (Weiter-)entwicklung unser aktuellen Softwarepakete für Julia, besonders im Bereich Graph Neural Networks. Dabei gibt es immer aktuelle...","categories": [],
+        "tags": ["sb","sm","hiwi"],
+        "url": "/trommer/software.html",
+        "teaser": null
+      },{
+        "title": "Datengetriebene Beobachter",
+        "excerpt":"Du hast Kenntnisse in der Regelungstechnik und Interesse an der Integration von KI-Methoden in die Regelungs-/Steuerungsmethoden? In der Regelungstechnik ist ein Beobachter ein System, das aus bekannten Eingangsgrößen (z.B. Stellgrößen oder messbaren Störgrößen) und Ausgangsgrößen (Messgrößen) eines beobachteten Bezugssystems nicht messbare Größen (Zustände) rekonstruiert. Für nichtlineare Systeme mit teilweiser unbekannter...","categories": [],
+        "tags": ["sa","ma"],
+        "url": "/zhang/Data-Driven%20Observer.html",
+        "teaser": null
+      },{
+        "title": "Datengetriebene Model Predictive Control",
+        "excerpt":"Du hast Kenntnisse in der Regelungstechnik und Interesse an der Integration von KI-Methoden in die Regelungs-/Steuerungsmethoden? Model Predictive Control ist eine Reglungstechnik, bei der ein System – wie z. B. ein Auto, eine Drohne oder eine Industrieanlage – in die Zukunft “blickt”, um zu entscheiden, was es als Nächstes tun...","categories": [],
+        "tags": ["sa","ma"],
+        "url": "/zhang/Data-Driven%20MPC.html",
+        "teaser": null
+      }]
