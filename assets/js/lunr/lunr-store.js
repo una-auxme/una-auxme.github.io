66f145a49cd3538dc54ceea5ff2b110e1fcae257
JoKircher <JoKircher@users.noreply.github.com>
2025-12-05 08:05:27 +0000

    Deploy site built from commit de65c8cd3cd8d777de4f1132f541aeae9652aba0

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "Empirical evaluation of training methods for Balanced Neural ODEs",
+        "excerpt":"Die Energiewende stellt uns vor die Herausforderung, Energie effizient zu nutzen und flexibel auf die schwankende Erzeugung aus erneuerbaren Quellen zu reagieren. Hochaufgelöste Simulationen physikalischer Systeme (z. B. CFD) sind für die Optimierung dieser Prozesse unverzichtbar – aber oft zu rechenintensiv für den praktischen Einsatz in iterativen Optimierungen. Die Lösung: Model...","categories": [],
+        "tags": ["pm","fm"],
+        "url": "/aka/training.html",
+        "teaser": null
+      },{
+        "title": "Konditionierung von Diffusion Modellen",
+        "excerpt":"Du findest generative Modelle wie Stable Diffusion oder DALL-E, die anhand von Text detailreiche Bilder erzeugen können, spannend und hast Interesse, dich in aktuelle Machine-Learning-Themen einzuarbeiten? In dieser Arbeit hast du die Möglichkeit, Methoden wie Stable Diffusion und SV3D genauer kennenzulernen. Stable Diffusion ermöglicht es, Bilder anhand von Text oder...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/Diffusion.html",
+        "teaser": null
+      },{
+        "title": "Fine-tuning von Large Reconstruction Models für die 3D Objektgenerierung",
+        "excerpt":"Kann man per Knopfdruck 3D-Objekte erzeugen? Generative Modelle wie Stable Diffusion oder DALL-E können bereits beeindruckende Bilder anhand einer Beschreibung auf Knopfdruck erzeugen. Das wäre auch für 3D-Objekte interessant, und tatsächlich lassen sich Methoden der Bildgenerierung auch zur Erzeugung von 3D-Objekten nutzen. Allerdings sind die Ergebnisse oft noch nicht so...","categories": [],
+        "tags": ["pm","ma"],
+        "url": "/boborzi/LRM.html",
+        "teaser": null
+      },{
+        "title": "Vergleich von Optimal-Control-Toolchains in Julia",
+        "excerpt":"Am Lehrstuhl für Mechatronik beschäftigen wir uns regelmäßig mit Optimal-Control-Problemen, insbesondere im Kontext modellprädiktiver Regelung (MPC). Für die Umsetzung solcher Algorithmen ist die Programmiersprache Julia besonders interessant. Es existieren bereits mehrere Pakete und Workflows zur Lösung von Optimal-Control-Problemen, z. B.: OptimalControl.jl ModelPredictiveControl.jl NLOptControl.jl … Ziel der Arbeit ist es, einen systematischen...","categories": [],
+        "tags": ["pm","fm","sa","sb","ba"],
+        "url": "/hoepfner/mpc_de.html",
+        "teaser": null
+      },{
+        "title": "Comparison of Optimal Control Toolchains in Julia",
+        "excerpt":"Topic for Student Thesis: Comparison of Optimal Control Toolchains in Julia At the Chair of Mechatronics, we regularly deal with optimal control problems, especially in the context of model predictive control (MPC). For implementing such algorithms, the Julia programming language is of particular interest. Several packages and workflows for solving...","categories": [],
+        "tags": ["pm","fm","sa","sb","ba"],
+        "url": "/hoepfner/mpc_en.html",
+        "teaser": null
+      },{
+        "title": "Differenzierbarkeit von Nachbarschaftssuche und der Earth Mover's Distance",
+        "excerpt":"Im Kontext von Deep Learning und Neuronalen Netzen ist für den Trainingsprozess unerlässlich, dass der Code vollständig differenzierbar ist, um die Gradienten für die Trainingsupdates berechnen zu können. Bei zwei Teilen ist dies aktuell noch nicht so. In vorhergegangen studentischen Arbeiten wurde eine GPU-fähige Nachbarschaftssuche und eine Lossfunktion basierend auf...","categories": [],
+        "tags": ["pm","ma","fm","ba"],
+        "url": "/kircher/AD_ready.html",
+        "teaser": null
+      },{
+        "title": "Messung von Sprühbildern von Partikelsimulationen",
+        "excerpt":"Diese studentische Arbeit befasst sich mit der Erfassung von Sprühbildern in Partikelsystemen. Eine mögliche Validierung von Düsensimulationen ist die Betrachtung von Sprühbildern. Dafür wird die Masseverteilung in einem gewissen Abstand gemessen und mit realen Daten verglichen. Um dies zu erreichen werden die bereitgestellten Simulationsdaten aufbereitet, um wichtige Größen aus den...","categories": [],
+        "tags": ["sm","fm","sb"],
+        "url": "/kircher/KPI_Spr%C3%BChbilder.html",
+        "teaser": null
+      },{
+        "title": "Vorhersage von Leistungsfähigkeit von Menschen durch GPT Modelle",
+        "excerpt":"Am Lehrstuhl für Mechatronik untersuchen wir Mensch-Roboter Interaktion von einem besonderen Blickwinkel. Jeder Mensch besitzt Fähigkeiten. Diese lassen sich bereits über arbeitsmedizinische Dokumentationsverfahren bewerten. Gleichsam lassen sich Anforderungen an einen Arbeitsprozess definieren. Über den Vergleich von Fähigkeiten und Anforderungen lässt sich dann auswerten, in welchen Bereichen eine Person eingesetzt werden...","categories": [],
+        "tags": ["ma"],
+        "url": "/mandischer/capability_gpt.html",
+        "teaser": null
+      },{
+        "title": "Klassifikation von Griffvariaten für Roboterhände",
+        "excerpt":"Am Lehrstuhl für Mechatronik untersuchen wir Mensch-Roboter Interaktion in verschiedenen Anwendungsfällen. Dabei werden Roboterbewegungen per Learning from Demonstration (d.h. durch Nachahmung des Menschen durch den Roboter) angelernt. Einen zunehmend wichtigen Faktor spielen dabei für den Menschen ausgelegte Prozesse, sei es das Schraubeneindrehen in der Arbeitswelt oder Lebensmittel einräumen im Haushalt....","categories": [],
+        "tags": ["pm","ba","ma"],
+        "url": "/mandischer/grip_detection.html",
+        "teaser": null
+      },{
+        "title": "Reharobotik für Armfunktionstraining nach Schlaganfall",
+        "excerpt":"Gemeinsam mit dem Therapiezentrum Burgau wollen wir zeitnah mit der Entwicklung eines Reharoboters starten, der zur teilautomatisierten Therapie im Armfunktionstraining nach Schlaganfall eingesetzt werden soll. Der Roboter verstärkt und führt dabei die Bewegung der Patientinnen und Patienten mit Armparese (min. halbseitige Lähmung der oberen Extremitäten) und entlastet dadurch die Therapeutinnen...","categories": [],
+        "tags": ["ma"],
+        "url": "/mandischer/reha.html",
+        "teaser": null
+      },{
+        "title": "SciML against AML",
+        "excerpt":"Jeder sechste Mensch stirbt laut WHO an Krebs. Dem gegenüber steht der stetige wissenschalftliche Fortschritt auf dem Gebiet der Krebsforschung, sodass viele Krebsarten geheilt werden können wenn sie frühzeitig erkannt und effektiv behandelt werden. Der Lehrstuhl Mechatronik startet 2025 zusammen mit dem Universitätsklinikum Augsburg Forschung im Bereich der Akuten myeloischen...","categories": [],
+        "tags": ["sb","sm"],
+        "url": "/thummerer/aml.html",
+        "teaser": null
+      },{
+        "title": "Software für Scientific Machine Learning (SciML)",
+        "excerpt":"Wenn mächtige, aber schwerfällige Simulationsmodelle aus der Industrie und nagelneue, flinke Methoden aus dem maschinellen Lernen zusammen kommen, dann kollidideren Welten - dank FMI.jl und FMIFlux.jl bekommt man davon aber (fast) nichts mit. Damit das so bleibt - und wir im Idealfall das “fast” im vorhergehenden Satz bald komplett streichen...","categories": [],
+        "tags": ["pm","fm"],
+        "url": "/thummerer/sciml.html",
+        "teaser": null
+      },{
+        "title": "Weiterentwicklung unserer Softwarepakete für Graph Neural Networks",
+        "excerpt":"Die Programmiersprache Julia wächst aktuell stark in ihrer Anwendung sowohl in der Forschung als auch in der Industrie bei großen Unternehmen wie beispielsweise ASML und Bosch. Dabei arbeitet unser Lehrstuhl aktiv an der (Weiter-)entwicklung unser aktuellen Softwarepakete für Julia, besonders im Bereich Graph Neural Networks. Dabei gibt es immer aktuelle...","categories": [],
+        "tags": ["pm","ba","ma","hiwi"],
+        "url": "/trommer/software.html",
+        "teaser": null
+      },{
+        "title": "Weiterentwicklung der Methodik von Graph Neural Networks",
+        "excerpt":"Graph Neural Networks (GNNs) eignen sich hervorragend als Surrogatmodelle für graphbasierte Simulationen, denen komplexe physikalische Phänomene zugrunde liegen. Durch das Lernen lokaler Zusammenhänge können sie auch bei sich ändernden Geometrien oder Topologien des Graphen weiterverwendet werden, ohne dass ein erneutes Training erforderlich ist. In dieser Arbeit soll die Methodik des...","categories": [],
+        "tags": ["pm","ba","ma","hiwi"],
+        "url": "/trommer/methods.html",
+        "teaser": null
+      },{
+        "title": "Datengetriebene Beobachter",
+        "excerpt":"Du hast Kenntnisse in der Regelungstechnik und Interesse an der Integration von KI-Methoden in die Regelungs-/Steuerungsmethoden? In der Regelungstechnik ist ein Beobachter ein System, das aus bekannten Eingangsgrößen (z.B. Stellgrößen oder messbaren Störgrößen) und Ausgangsgrößen (Messgrößen) eines beobachteten Bezugssystems nicht messbare Größen (Zustände) rekonstruiert. Für nichtlineare Systeme mit teilweiser unbekannter...","categories": [],
+        "tags": ["sa","ma"],
+        "url": "/zhang/Data-Driven%20Observer.html",
+        "teaser": null
+      },{
+        "title": "Datengetriebene Model Predictive Control",
+        "excerpt":"Du hast Kenntnisse in der Regelungstechnik und Interesse an der Integration von KI-Methoden in die Regelungs-/Steuerungsmethoden? Model Predictive Control ist eine Reglungstechnik, bei der ein System – wie z. B. ein Auto, eine Drohne oder eine Industrieanlage – in die Zukunft “blickt”, um zu entscheiden, was es als Nächstes tun...","categories": [],
+        "tags": ["sa","ma"],
+        "url": "/zhang/Data-Driven%20MPC.html",
+        "teaser": null
+      }]
